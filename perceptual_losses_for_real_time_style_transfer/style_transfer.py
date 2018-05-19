c40881df82d966406fb01c7a92e60c43c2528bac
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-20 00:43:17 +0300

    Update model

--- perceptual_losses_for_real_time_style_transfer/style_transfer.py
+++ perceptual_losses_for_real_time_style_transfer/style_transfer.py
@@ -43,48 +43,46 @@ class StyleTransfer():
     self.no_epochs = no_epochs
     self.batch_size = batch_size
 
-  def _get_content_loss(self, content_img_layers, noise_img_layers):
+  def _get_content_loss(self, content_layer, noise_layer):
     content_loss = tf.constant(0.0)
-    for content_layer_name in self.content_layers:
-      content_loss = content_loss \
-                     + tf.reduce_sum(tf.square(
-                        content_img_layers[content_layer_name] \
-                        - noise_img_layers[content_layer_name]))
-      content_loss = tf.scalar_mul(1.0 / (2.0
-                                   * tf.cast(tf.shape(content_img_layers[content_layer_name])[1],
-                                             tf.float32)
-                                   * tf.cast(tf.shape(content_img_layers[content_layer_name])[2],
-                                             tf.float32)
-                                   * tf.cast(tf.shape(content_img_layers[content_layer_name])[3],
-                                             tf.float32)),
-                                   content_loss)
+    content_loss = content_loss \
+                   + tf.reduce_sum(tf.square(content_layer \
+                                             - noise_layer))
+    content_loss = tf.scalar_mul(1.0 / (2.0
+                                 * tf.cast(tf.shape(content_layer)[1],
+                                           tf.float32)
+                                 * tf.cast(tf.shape(content_layer)[2],
+                                           tf.float32)
+                                 * tf.cast(tf.shape(content_layer)[3],
+                                           tf.float32)),
+                                 content_loss)
 
     return content_loss
 
-  def _get_style_loss(self, style_img_layers, noise_img_layers):
+  def _get_style_loss(self, style_layer, noise_layer):
     style_loss = tf.constant(0.0)
 
-    for i, style_layer_name in enumerate(self.style_layers):
-      channels_matrix_style_img = tf.reshape(style_img_layers[style_layer_name],
-                    [-1, tf.shape(style_img_layers[style_layer_name])[3]])
-      channels_matrix_noise_img = tf.reshape(noise_img_layers[style_layer_name],
-                    [-1, tf.shape(noise_img_layers[style_layer_name])[3]])
+    channels_matrix_style_img = tf.reshape(style_layer,
+                                           [-1, tf.shape(style_layer)[3]])
+    channels_matrix_noise_img = tf.reshape(noise_layer,
+                                           [-1, tf.shape(noise_layer)[3]])
 
-      gram_matrix_style = tf.matmul(tf.transpose(channels_matrix_style_img),
-                            channels_matrix_style_img)
-      gram_matrix_noise = tf.matmul(tf.transpose(channels_matrix_noise_img),
-                            channels_matrix_noise_img)
+    gram_matrix_style = tf.matmul(tf.transpose(channels_matrix_style_img),
+                                  channels_matrix_style_img)
+    gram_matrix_noise = tf.matmul(tf.transpose(channels_matrix_noise_img),
+                                  channels_matrix_noise_img)
 
-      El = tf.reduce_sum(tf.square(gram_matrix_style - gram_matrix_noise))
-      El = tf.scalar_mul(1.0 / (4.0
-                         * tf.square(tf.cast(tf.shape(style_img_layers[style_layer_name])[1],
-                                             tf.float32))
-                         * tf.square(tf.cast(tf.shape(style_img_layers[style_layer_name])[2],
-                                             tf.float32))
-                         * tf.square(tf.cast(tf.shape(style_img_layers[style_layer_name])[3],
-                                             tf.float32))),
-                         El)
-      style_loss = style_loss + tf.scalar_mul(self.style_layers_w[i], El)
+    El = tf.reduce_sum(tf.square(gram_matrix_style - gram_matrix_noise))
+    El = tf.scalar_mul(1.0 / (4.0
+                       * tf.square(tf.cast(tf.shape(style_layer)[1],
+                                           tf.float32))
+                       * tf.square(tf.cast(tf.shape(style_layer)[2],
+                                           tf.float32))
+                       * tf.square(tf.cast(tf.shape(style_layer)[3],
+                                           tf.float32))),
+                       El)
+
+    style_loss = style_loss + El
 
     return style_loss
 
@@ -108,15 +106,23 @@ class StyleTransfer():
     if self.model_name == 'vgg19':
       self.noise_img = self.noise_img - self.vgg_means # normalize img for vgg19
 
-    _, self.content_img_layers = self.model.run(self.content_img_vgg)
-    _, self.style_img_layers = self.model.run(self.style_img)
-    _, self.noise_img_layers = self.model.run(self.noise_img)
+    self.content_loss = tf.constant(0.0)
+    for content_layer_name in self.content_layers:
+      content_layer = self.model.run(self.content_img_vgg, content_layer_name)
+      noise_layer = self.model.run(self.noise_img, content_layer_name)
+      self.content_loss = self.content_loss \
+                          + self._get_content_loss(content_layer, noise_layer)
 
-    self.content_loss = self._get_content_loss(self.content_img_layers,
-                                               self.noise_img_layers)
-    self.style_loss = self._get_style_loss(self.style_img_layers,
-                                           self.noise_img_layers)
-    self.total_loss = self.alfa * self.content_loss + self.beta * self.style_loss
+    self.style_loss = tf.constant(0.0)
+    for i, style_layer_name in enumerate(self.style_layers):
+      style_layer = self.model.run(self.style_img, style_layer_name)
+      noise_layer = self.model.run(self.noise_img, style_layer_name)
+      self.style_loss = self.style_loss \
+                          + tf.scalar_mul(self.style_layers_w[i],
+                                self._get_style_loss(style_layer, noise_layer))
+
+    self.total_loss = self.alfa * self.content_loss \
+                      + self.beta * self.style_loss
 
     tf.summary.scalar('content_loss', self.content_loss)
     tf.summary.scalar('style_loss', self.style_loss)
@@ -128,7 +134,9 @@ class StyleTransfer():
     self.optim = tf.train.AdamOptimizer(learning_rate=self.learning_rate,
         name='adam_optimizer').minimize(self.total_loss, var_list=self.var_list)
 
-  def train(self):
+  def train(self,
+            style_img_path,
+            save_img_path):
     summ = tf.summary.merge_all()
     with tf.Session() as sess:
       sess.run(tf.global_variables_initializer())
@@ -140,7 +148,9 @@ class StyleTransfer():
 
       y_batch = []
       for _ in range(self.batch_size):
-        y_batch.append(ut.get_img('starry_night.jpg', width=256, height=256))
+        y_batch.append(ut.get_img(style_img_path,
+                                  width=self.style_img_width,
+                                  height=self.style_img_height))
       y_batch = np.array(y_batch).astype(np.float32)
 
       ep = 0
@@ -149,8 +159,8 @@ class StyleTransfer():
         ut = Utils()
         while True:
           x_batch_transform, batch_end = ut.next_batch_train(self.batch_size,
-                                                   width=256,
-                                                   height=256,
+                                                   width=self.content_img_width,
+                                                   height=self.content_img_height,
                                                    model='transform_net')
           x_batch_vgg = ut.normalize_img(ut.denormalize_img(x_batch_transform,
                                                             model='transform_net'))
@@ -170,8 +180,9 @@ class StyleTransfer():
 
           if i % 10 == 0:
             ut.save_img(ut.denormalize_img(out_img[0]), 'images/img' + str(i) + '.jpg')
+            ut.save_img(ut.denormalize_img(out_img[0]), save_img_path + '/img' + str(i) + '.jpg')
             ut.save_img(ut.denormalize_img(x_batch_transform[0], model='transform_net'),
-                        'images/img' + str(i) + 'c.jpg')
+                        save_img_path + '/img' + str(i) + 'c.jpg')
 
             s = sess.run(summ,
                          feed_dict={self.content_img_transform: x_batch_transform,
