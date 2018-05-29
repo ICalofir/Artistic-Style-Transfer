3c799f69afdbfb99e777d6f2a6b4c24a402bd3a3
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-29 23:54:14 +0300

    Add gcloud function to train on cloud

---
+++ gcloud/main.sh
@@ -0,0 +1,15 @@
+DIRECTORY="trainer"
+if [ -d "$DIRECTORY" ]; then
+  rm -rf $DIRECTORY
+fi
+mkdir $DIRECTORY
+
+cp ../main.py $DIRECTORY/main.py
+cp ../utils.py $DIRECTORY/utils.py
+cp ../model.py $DIRECTORY/model.py
+cp ../a_neural_algorithm_of_artistic_style/anaoas_style_transfer.py $DIRECTORY/anaoas_style_transfer.py
+cp ../perceptual_losses_for_real_time_style_transfer/plfrtst_style_transfer.py $DIRECTORY/plfrtst_style_transfer.py
+cp ../perceptual_losses_for_real_time_style_transfer/dataset.py $DIRECTORY/dataset.py
+cp ../deep_photo_style_transfer/dpst_style_transfer.py $DIRECTORY/dpst_style_transfer.py
+cp ../conv_nets/transform_net.py $DIRECTORY/transform_net.py
+cp ../conv_nets/vgg19.py $DIRECTORY/vgg19.py
