dc833f99350b3622714a4eefd328b3206dcb51a2
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-29 16:40:23 +0300

    Update gen laplacian

--- deep_photo_style_transfer/gen_laplacian/gen_laplacian.m
+++ deep_photo_style_transfer/gen_laplacian/gen_laplacian.m
@@ -2,11 +2,14 @@ pkg load image
 
 addpath matting/
 addpath gaimc/
-N = 1;
+N = 7;
+
+prefix = '../../images/content/d_content';
+img_type = '.png';
+laplacian_name = 'd_laplacian';
 
 for i = 1:N
-    prefix = '../../ionut_resized';
-    in_name = [prefix '.jpg'];
+    in_name = [prefix int2str(i) img_type];
 
     input = im2double(imread(in_name));
     [h w c] = size(input);
@@ -15,13 +18,15 @@ for i = 1:N
 
     n = nnz(A);
     [Ai, Aj, Aval] = find(A);
-    CSC = [Ai, Aj, Aval];
-    %save(['Input_Laplacian_3x3_1e-7_CSC' int2str(i) '.mat'], 'CSC');
+    % CSC = [Ai, Aj, Aval];
+    % save([laplacian_name int2str(i) '.mat'], 'CSC');
 
     [rp ci ai] = sparse_to_csr(A);
     Ai = sort(Ai);
     Aj = ci;
     Aval = ai;
     CSR = [Ai, Aj, Aval];
-    save('-v6', ['Input_Laplacian_3x3_1e-7_CSR' int2str(i) '.mat'], 'CSR'); % save -6 laplacian.mat CSR
+    save('-v6', [laplacian_name int2str(i) '.mat'], 'CSR');
+
+    clear A Ai Aj Aval CSR ai c ci h in_name input n rp w;
 end
