c904d84b7e43c842a58d09cb70d34fe3a58fef4f
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-31 00:06:29 +0300

    Add function to resize with ratio

--- utils.py
+++ utils.py
@@ -1,7 +1,10 @@
 import cv2
 import numpy as np
 
-from perceptual_losses_for_real_time_style_transfer.dataset import Dataset
+try:
+  from perceptual_losses_for_real_time_style_transfer.dataset import Dataset
+except ImportError: #gcloud
+  from dataset import Dataset
 
 class Utils():
   def __init__(self,
@@ -11,7 +14,7 @@ class Utils():
       self.ds = Dataset(data_path)
     self.vgg_means = [103.939, 116.779, 123.68] # BGR
 
-  def next_batch_train(self, n_batch=None, width=257, height=257, model='vgg'):
+  def next_batch_train(self, n_batch=None, width=224, height=224, model='vgg'):
     x_batch = []
 
     x_train, batch_end = self.ds.get_train_batch(n_batch)
@@ -23,7 +26,7 @@ class Utils():
 
     return x_batch.astype(np.float32), batch_end
 
-  def next_batch_val(self, n_batch=None, width=257, height=257, model='vgg'):
+  def next_batch_val(self, n_batch=None, width=224, height=224, model='vgg'):
     x_batch = []
 
     x_val = self.ds.get_val_batch(n_batch)
@@ -35,9 +38,12 @@ class Utils():
 
     return x_batch.astype(np.float32)
 
-  def get_img(self, img_name, width=257, height=257, model='vgg'):
+  def get_img(self, img_name, width=224, height=224, model='vgg'):
     img = cv2.imread(img_name)
-    img = self.resize_img(img, width, height)
+
+    if width != -1 and height != -1:
+      img = self.resize_img(img, width, height)
+
     img = self.normalize_img(img, model)
 
     return img
@@ -46,6 +52,32 @@ class Utils():
     img = cv2.resize(img, (width, height))
 
     return img
+
+  def resize_with_ratio(self, width=None, height=None, size=None):
+    if (size is not None or size != -1) and height > width:
+      new_height = size
+      new_width = int((width * new_height)
+                      / height)
+    elif (size is not None or size != -1):
+      new_width = size
+      new_height = int((height * new_width)
+                       / width)
+    elif height is not None and width is None:
+      new_height = height
+      new_width = int((width * new_height)
+                      / height)
+    elif height is None and width is not None:
+      new_width = width
+      new_height = int((height * new_width)
+                       / width)
+    elif size is not None:
+      new_height = height
+      new_width = width
+    else:
+      new_height = None
+      new_width = None
+
+    return new_height, new_width
 
   def normalize_img(self, img, model='vgg'):
     img = img.astype(np.float32)
