ad8bc2d53caf8ad2cb9ac002d932aad000df5ab4
Ionut Calofir <calofir.ionut@yahoo.com>
2018-05-31 22:57:59 +0300

    update main

--- main.py
+++ main.py
@@ -182,32 +182,20 @@ if __name__ == '__main__':
   elif args.method == 'plfrtst':
     if args.train:
       ut = Utils()
-      content_img_path = args.content_img_path or 'images/content/content1.jpg'
       style_img_path = args.style_img_path or 'images/style/style1.jpg'
 
       s = tf.InteractiveSession()
-      content_img_bytes = tf.read_file(content_img_path)
       style_img_bytes = tf.read_file(style_img_path)
 
-      content_img_np = np.fromstring(content_img_bytes.eval(), np.uint8)
       style_img_np = np.fromstring(style_img_bytes.eval(), np.uint8)
       s.close()
 
-      content_img = ut.get_img(content_img_np,
-                               width=-1,
-                               height=-1)
       style_img = ut.get_img(style_img_np,
                              width=-1,
                              height=-1)
-      content_img_height = content_img.shape[0]
-      content_img_width = content_img.shape[1]
       style_img_height = style_img.shape[0]
       style_img_width = style_img.shape[1]
 
-      args.content_img_height, args.content_img_width = \
-          ut.resize_with_ratio(height=args.content_img_height or content_img_height,
-                               width=args.content_img_width or content_img_width,
-                               size=args.content_img_size)
       args.style_img_height, args.style_img_width = \
           ut.resize_with_ratio(height=args.style_img_height or style_img_height,
                                width=args.style_img_width or style_img_width,
@@ -217,11 +205,13 @@ if __name__ == '__main__':
           model_name=args.model_name or 'vgg19',
           tensorflow_model_path=args.tensorflow_model_path
               or 'pretrained_models/vgg19/model/tensorflow/conv_wb.pkl',
-          content_img_height=args.content_img_height or 224,
-          content_img_width=args.content_img_width or 224,
+          data_path=args.data_path
+              or 'perceptual_losses_for_real_time_style_transfer/dataset',
+          content_img_height=args.content_img_height or 256,
+          content_img_width=args.content_img_width or 256,
           content_img_channels=args.content_img_channels or 3,
-          style_img_height=args.style_img_height or 224,
-          style_img_width=args.style_img_width or 224,
+          style_img_height=args.style_img_height or 256,
+          style_img_width=args.style_img_width or 256,
           style_img_channels=args.style_img_channels or 3,
           content_layers=args.content_layers or ['conv4_2'],
           style_layers=args.style_layers
