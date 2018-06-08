8cb29c6245c2ace2ade7347a2c858967db5468d4
Ionut Calofir <calofir.ionut@yahoo.com>
2018-06-08 20:00:46 +0300

    Fix noise img path

--- gcloud/anaoas_trainer.sh
+++ gcloud/anaoas_trainer.sh
@@ -15,7 +15,7 @@ LEARNING_RATE=$5
 NUM_ITERS=$6
 CONTENT_IMG_PATH=gs://$BUCKET_NAME/images/content/$7
 STYLE_IMG_PATH=gs://$BUCKET_NAME/images/style/$8
-NOISE_IMG_PATH=gs://$BUCKET_NAME/images/style/$9
+NOISE_IMG_PATH=gs://$BUCKET_NAME/images/content/$9
 OUTPUT_IMG_PATH=gs://$BUCKET_NAME/results/anaoas/alfa_${ALFA}_beta_${BETA}_gamma_${GAMMA}_lr_${LEARNING_RATE}
 TENSORBOARD_PATH=gs://$BUCKET_NAME/tensorboard/tensorboard_anaoas/alfa_${ALFA}_beta_${BETA}_gamma_${GAMMA}_lr_${LEARNING_RATE}
 
